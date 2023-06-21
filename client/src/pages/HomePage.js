8a5622b8135c4bd6bdd767495ed55dcd14bc32bd
maudelinDS <maudelin.delescaut@jobtrek.ch>
2023-06-21 09:15:41 +0200

    TEST

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,25 @@
+import React from "react";
+//import AppCrudStudent from "../AppCrudStudent";
+import Section from "../Sections";
+import Image from '../images/JOBK_Img_HeaderMobile_Home.png';
+import NavBar from "../appBar";
+import Tabs from "../Tabs"; // Import using relative path
+
+const Home = () => {
+
+    return <div style={{background: 'rgba(91,97,100,0.15)'}}>
+        <NavBar/>
+        <Section/>
+
+
+
+
+
+
+
+    </div>
+
+
+};
+
+export default Home;
